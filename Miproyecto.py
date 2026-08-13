27197c62e478477ad7ca95aba615ac3df0a14091
leidy <janethvelas.35@gmail.com>
2026-08-12 23:48:31 -0500

    Añado la bienvenida

--- Miproyecto.py
+++ Miproyecto.py
@@ -1,3 +1,9 @@
 print("inicio del proyecto") 
 
 ## este es un archivo de ejemplo para el proyecto 
+
+## basado en el readme del proyecto
+
+## juego de zoduko 
+
+print("te damos la bienvenida a nuestro juego de zoduko") 
